9a110567b14f89b04c0bd9e2b4f6b4e6ae762ed0
Menno Kramer <mennockramer@gmail.com>
2023-09-18 18:25:34 +0100

    Post-Elysium CV update + other tweaks

--- footer.js
+++ footer.js
@@ -1,5 +1,16 @@
-document.write('\
-\
-Copyright me, but GPLv3 unless stated otherwise.\
-\
-');
+var date;
+
+fetch('https://api.github.com/repos/mennockramer/mennockramer.github.io/commits/main')
+  .then(res => res.json())
+  .then(data => {
+    date = data['commit']['author']['date'].split("T")[0];
+   })
+  .then(() => {
+    document.querySelector("footer").innerHTML='\
+      \
+      Last Updated '.concat(date).concat('\
+      <br>\
+      Copyright me, but GPLv3 unless stated otherwise.\
+      \
+      ');
+   });
--- nav.js
+++ nav.js
@@ -5,8 +5,10 @@ document.write('\
             <td class="nav"><a href="index.html">Home</a></td>\
             <td class="nav"><a href="CV Menno Kramer.pdf">CV</a></td>\
             <td class="nav"><a href="projects.html">Projects</a></td>\
+            <td class="nav"><a href="mailto:mennockramer'.concat('@gmail.com">Email</a></td>\
+            <td class="nav"><a href="https://github.com/mennockramer">Github</a></td>\
         </tr>\
     </table>\
     <br>\
 \
-');
+'));
