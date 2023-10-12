4ab4403f84252474121ec0bc458f11c88fbe04a8
Menno Kramer <mennockramer@gmail.com>
2023-10-12 11:58:16 +0100

    Post-Elysium date update and CV rename for URL readability

--- nav.js
+++ nav.js
@@ -3,7 +3,7 @@ document.write('\
     <table>\
         <tr>\
             <td class="nav"><a href="index.html">Home</a></td>\
-            <td class="nav"><a href="CV Menno Kramer.pdf">CV</a></td>\
+            <td class="nav"><a href="CV_Menno_Kramer.pdf">CV</a></td>\
             <td class="nav"><a href="projects.html">Projects</a></td>\
             <td class="nav"><a href="mailto:mennockramer'.concat('@gmail.com">Email</a></td>\
             <td class="nav"><a href="https://github.com/mennockramer">Github</a></td>\
